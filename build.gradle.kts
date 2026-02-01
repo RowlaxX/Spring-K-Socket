b35c730b0762ed1073033ed3eedea18ff31bcd20
RowlaxX <theo.linder1@gmail.com>
2026-02-01 20:45:25 +0100

    Added `org.jetbrains:annotations` as a compile-only dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-websocket-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    compileOnly("org.jetbrains:annotations:24.1.0")
 }
 
 publishing {
